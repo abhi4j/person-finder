bcc2dbc609bc3e091757e42867bc59462cc3b159
abhi4j <abhi559416@yahoo.com>
2014-04-04 04:28:30 +0530

    interface for info seeker

---
+++ seeker.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>seeker</title>
+<style type="text/css">
+
+a:link {
+	color: #F0F0F0;
+}
+.style2 {
+	font-family: Geneva, Arial, Helvetica, sans-serif;
+	font-style: italic;
+	font-size: 36px;
+	color: #CC3300;
+}
+.style3 {
+	font-family: "Courier New", Courier, monospace;
+	font-size: 18px;
+	color: #CC3300;
+}
+body {
+	background-color: #33CCFF;
+}
+.style7 {
+	font-size: 24px;
+	color: #003399;
+}
+.style13 {
+	font-family: "Courier New", Courier, monospace;
+	font-size: 18px;
+	color: #CC3333;
+}
+.style14 {
+	font-size: 36px;
+	color: #CC0033;
+}
+</style>
+
+
+</head>
+
+<body>
+<table width="97%" border="2" cellspacing="5px" cellpadding="5px">
+  <tr>
+    <th width="17%" height="116" scope="col"><img src="images/helicopter.jpg" width="101%" height="172"/></th>
+    <th width="83%" bordercolor="#FFFFFF" bgcolor="#00FFFF" scope="col" background="images/map.jpg"><h1 class="style2">PERSON FINDER</h1>
+    <p class="style2 style7">Lets bring them back..TOGETHER..!! </p></th>
+  </tr>
+</table>
+<p><br />
+  <br />
+    <span class="style13">We offer deep sypathy to you and offer to help in whatever manner we can.!! Our database size has grown up tremendously and hence chances are greater that you will be able to successfully reach your relatives. </span></p>
+<h1><em><span class="style14">Please provide whatever information you have about the person by filling up the below form.</span></em></h1>
+<center>
+<form name="f4" action="index.php">
+<input type="submit" value="HOME" /><br /><br /><br />
+</form>
+<form id="form1" name="form2" method="post" action="result.php">
+<table class="style3">
+<tr>
+<td>name of the person</td>
+<td><input type="text" name="name" /></td>
+</tr>
+
+<tr>
+<td>Your contact No.</td>
+<td><input type="text" name="phone" /></td>
+
+</tr>
+<tr>
+<td colspan="2" rowspan="2"><center><input type="submit" value="submit" /></center></td>
+</table>
+</form>
+</center>
+</form>
+<p><em></em></p>
+</body>
+</html>
