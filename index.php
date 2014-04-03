63c4759709aae801151beb07635cdaeb378ccaf2
abhi4j <abhi559416@yahoo.com>
2014-04-04 04:27:07 +0530

    Homre

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>HOME PAGE</title>
+<style type="text/css">
+<!--
+a:link {
+	color: #F0F0F0;
+}
+.style2 {
+	font-family: Geneva, Arial, Helvetica, sans-serif;
+	font-style: italic;
+	font-size: 36px;
+	color: #CC3300;
+}
+-->
+</style>
+<link href="mystyling.css" rel="stylesheet" type="text/css" />
+<script type="text/JavaScript">
+<!--
+
+</script>
+<style type="text/css">
+<!--
+.style3 {
+	font-family: "Courier New", Courier, monospace;
+	font-size: 18px;
+	color: #CC3300;
+}
+.style4 {
+	font-family: "Courier New", Courier, monospace;
+	font-size: 36px;
+	color: #CC3366;
+	font-weight: bold;
+}
+body {
+	background-color: #33CCFF;
+}
+.style6 {font-family: "Courier New", Courier, monospace; font-size: 18px; color: #CC3333; font-weight: bold; }
+.style7 {
+	font-size: 24px;
+	color: #003399;
+}
+.style8 {
+	font-size: 24px;
+	color: #3300CC;
+}
+.style9 {
+	font-size: 24px;
+	color: #333399;
+}
+-->
+</style>
+</head>
+
+<body onload="MM_preloadImages('images/3.jpg','images/2.jpg','images/5.jpg')">
+<table width="97%" border="2" cellspacing="5px" cellpadding="5px">
+  <tr>
+    <th width="17%" height="116" scope="col"><img src="images/helicopter.jpg" width="101%" height="172"/></th>
+    <th width="83%" bordercolor="#FFFFFF" bgcolor="#00FFFF" scope="col" background="images/map.jpg"><h1 class="style2">PERSON FINDER</h1>
+    <p class="style2 style7">Lets bring them back..TOGETHER..!! </p></th>
+  </tr>
+</table>
+
+
+
+<h2 class="style3">We at person finder are deeply concerned about the loss or the fear of it, for your loved ones.This concern has led us to make  this simple application to find the whereabouts of your dear ones. It is a little piece of help we are trying to offer.</h2>
+<h1 align="center" class="style4">HOW PERSON FINDER WORKS? </h1>
+<p align="left" class="style6">PERSON FINDER works with two interfaces,one for the people who have information regarding somebody on the disaster site.They can enter the information on the and if possible take a photo of them and add this also to the information.</p>
+<p align="left" class="style6">Second interface is for the people who are seeking whereabouts of their relatives.In this interface you are required to give out whatever information you have about the person you are searching for.This will be searched in our database and if your information matches you will be provided with their whereabouts and the contacts nos. to get in touch with them.</p>
+<p align="left" class="style6">&nbsp;	</p>
+<form id="form1" name="form1" method="post" action="">
+  <center><p>
+    <input name="giver" type="button" class="style4" value="DO YOU HAVE INFO?" onClick="window.location='giver.php'" />
+   &nbsp;&nbsp;&nbsp;
+  <input name="seeker" type="button" class="style4" value="DO YOU WANT INFO?" onClick="window.location='seeker.php'" /><br />
+   
+    </p>
+	</center>
+</form>
+<p align="left" class="style6">&nbsp;</p>
+</body>
+</html>
