73923031eec42799bfc03a26ee04da7370b99308
abhi4j <abhi559416@yahoo.com>
2014-04-06 18:35:22 +0530

    successful query

---
+++ thanx.php
@@ -0,0 +1,60 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Thank You</title>
+<style type="text/css">
+
+a:link {
+	color: #F0F0F0;
+}
+.style2 {
+	font-family: Geneva, Arial, Helvetica, sans-serif;
+	font-style: italic;
+	font-size: 36px;
+	color: #CC3300;
+}
+body {
+	background-color: #33CCFF;
+}
+.style7 {
+	font-size: 24px;
+	color: #003399;
+}
+.style10 {
+	font-size: 36px;
+	color: #336666;
+	font-weight: bold;
+}
+.style11 {color: #990000}
+</style>
+
+
+</head>
+
+<body>
+<table width="97%" border="2" cellspacing="5px" cellpadding="5px">
+  <tr>
+    <th width="17%" height="116" scope="col"><img src="images/helicopter.jpg" width="101%" height="172"/></th>
+    <th width="83%" bordercolor="#FFFFFF" bgcolor="#00FFFF" scope="col" background="images/map.jpg"><h1 class="style2">PERSON FINDER</h1>
+    <p class="style2 style7">Lets bring them back..TOGETHER..!! </p></th>
+  </tr>
+</table>
+<br />
+<br />
+<br />
+<center>
+<form name="f6" action="giver.php">
+<input type="submit" value="GO BACK" />
+</form>
+</center>
+<br />
+<br />
+<br />
+<center><h1 class="style10 style11"> THANK YOU!!<br/ >Your contribution is priceless.!!!.</h1>
+
+
+</center>
+</body>
+</html>
