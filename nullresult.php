ef979abb8591ecf712b64f638c979543214240ef
abhi4j <abhi559416@yahoo.com>
2014-04-17 14:55:43 +0530

    re result for your search

---
+++ nullresult.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<head>
+<title>Result</title>
+<style type="text/css">
+
+a:link {
+	color: #F0F0F0;
+}
+.style2 {
+	font-family: Geneva, Arial, Helvetica, sans-serif;
+	font-style: italic;
+	font-size: 36px;
+	color: #CC3300;
+}
+body {
+	background-color: #33CCFF;
+}
+.style7 {
+	font-size: 24px;
+	color: #003399;
+}
+.style14 {
+	font-size: 36px;
+	color: #CC0033;
+}
+.style3 {
+	font-family: "Courier New", Courier, monospace;
+	font-size: 18px;
+	color: #CC3300;
+}
+.style18 {color: #FFFFFF}
+</style>
+
+
+</head>
+
+<body>
+<table width="97%" border="2" cellspacing="5px" cellpadding="5px">
+  <tr>
+    <th width="17%" height="116" scope="col"><img src="images/helicopter.jpg" width="101%" height="172"/></th>
+    <th width="83%" bordercolor="#FFFFFF" bgcolor="#00FFFF" scope="col" background="images/map.jpg"><h1 class="style2">PERSON FINDER</h1>
+    <p class="style2 style7">Lets bring them back..TOGETHER..!! </p></th>
+  </tr>
+</table>
+<h2 align="center" class="style3">Sorry!! We don't have any information regarding the person you searched. We will email you as soon as anybody provides us information.</h2>
+</body>
+</html>
